6bd923449c5c0ab22668c500b7d3724eb5bb7745
rahinisrinivas <rahini.srinivas@elasticpath.com>
2024-05-09 13:18:15 -0400

    update ref links and fix formatting issues

--- src/components/api/HeroSectionAPIs.tsx
+++ src/components/api/HeroSectionAPIs.tsx
@@ -29,7 +29,7 @@ const PRODUCTS = [
   },
   {
     title: 'Commerce Extensions',
-    link: '/docs/api/commerce-extensions/commerce-extensions-introduction',
+    link: '/docs/api/commerceextensions/commerce-extensions-introduction',
     icon: BookDatabaseRegular,
     // lightImage: '/static/landing-page/hero/video-graphic.png',
     // darkImage: '/static/landing-page/hero/video-graphic-dark.png',
