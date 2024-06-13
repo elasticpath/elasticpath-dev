c084f108bc4da4de4d3ac1814d9b937b713e296a
Sam Blacklock <samblacklock@users.noreply.github.com>
2024-06-13 14:33:59 +0100

    feat: new homepage (#63)
    * feat: background and hero
    
    * feat: add apps section
    
    * feat: add CTA
    
    * feat: start testimonials
    
    * feat: api section
    
    * feat: content complete
    
    * feat: testimonials complete
    
    * feat: footer
    
    * feat: svg for blend
    
    * feat: use browseronly for devtools
    
    * feat: removing components to test build
    
    * feat: gitignore
    
    * feat: gitignore
    
    * feat: remove .idea files
    
    * fix: client side render
    
    * feat: apps content
    
    * feat: apps styling
    
    * feat: modern commeerce apis styling tweaked
    
    * feat: styles for tools
    
    * feat: styles for composer and success
    
    * feat: functional light mode
    
    * feat: swap code block for prism
    
    * feat: mobile sizing
    
    * feat: tweaks
    
    * feat: nav styling
    
    * feat: update bg
    
    * feat: commerce apps transparency
    
    * feat: blur nav
    
    * feat: rebuild yarn.lock
    
    * feat: add links to actions
    
    * fix: css rendering, list items
    
    * feat: tab tool ssection
    
    * fix: mobile styling issues
    
    * fix: update links
    
    * fix: correct opacity for mobile view
    
    ---------
    
    Co-authored-by: David Stover <56250584+davidmoltin@users.noreply.github.com>

--- src/components/homepage/HomeFooter.jsx
+++ src/components/homepage/HomeFooter.jsx
@@ -1,77 +1,85 @@
-import React, { useEffect, useState } from 'react';
-import Link from '@docusaurus/Link';
-import clsx from 'clsx';
-import { Linkedin, Youtube, Github } from '@styled-icons/boxicons-logos';
-import { XIcon } from '@site/src/icons';
-import BrowserOnly from '@docusaurus/BrowserOnly';
+import React, { useEffect, useState } from "react";
+import Link from "@docusaurus/Link";
+import clsx from "clsx";
+import { Linkedin, Youtube, Github } from "@styled-icons/boxicons-logos";
+import { XIcon } from "@site/src/icons";
+import BrowserOnly from "@docusaurus/BrowserOnly";
 
 const customers = [
   {
-    name: 'Customer Success Team',
-    href: 'https://www.elasticpath.com/customer-success/team',
+    name: "Customer Success Team",
+    href: "https://www.elasticpath.com/customer-success/team",
   },
   {
-    name: 'Global Services',
-    href: 'https://www.elasticpath.com/customer-success/global-services',
+    name: "Global Services",
+    href: "https://www.elasticpath.com/customer-success/global-services",
   },
   {
-    name: 'Support',
-    href: 'https://support.elasticpath.com/hc/en-us',
+    name: "Support",
+    href: "https://support.elasticpath.com/hc/en-us",
   },
-  { name: 'US East Login', href: 'https://useast.cm.elasticpath.com/' },
-  { name: 'EU West Login', href: 'https://euwest.cm.elasticpath.com/' },
+  { name: "US East Login", href: "https://useast.cm.elasticpath.com/" },
+  { name: "EU West Login", href: "https://euwest.cm.elasticpath.com/" },
 ];
 
 const partners = [
   {
-    name: 'Partner Program',
-    href: 'https://www.elasticpath.com/partners',
+    name: "Partner Program",
+    href: "https://www.elasticpath.com/partners",
   },
   {
-    name: 'Partner Directory',
-    href: 'https://www.elasticpath.com/partners/directory',
+    name: "Partner Directory",
+    href: "https://www.elasticpath.com/partners/directory",
   },
   {
-    name: 'Become a Partner',
-    href: 'https://www.elasticpath.com/partners#form',
+    name: "Become a Partner",
+    href: "https://www.elasticpath.com/partners#form",
   },
 ];
 
 const developers = [
-  { name: 'Technologies', href: 'https://www.elasticpath.com/products/architecture#technologies' },
-  { name: 'Dev Center', href: 'https://elasticpath.dev/?_gl=1*ugmkpl*_ga*ODk1MjQyNDE2LjE2NzgyMTYwNjU.*_ga_KMLE7BQGFN*MTcwMTk2NTE2NS4yOTMuMS4xNzAxOTY3NTcwLjU5LjAuMA..' },
-  { name: 'Free Trial', href: 'https://useast.cm.elasticpath.com/free-trial' },
+  {
+    name: "Technologies",
+    href: "https://www.elasticpath.com/products/architecture#technologies",
+  },
+  {
+    name: "Dev Center",
+    href: "https://elasticpath.dev/?_gl=1*ugmkpl*_ga*ODk1MjQyNDE2LjE2NzgyMTYwNjU.*_ga_KMLE7BQGFN*MTcwMTk2NTE2NS4yOTMuMS4xNzAxOTY3NTcwLjU5LjAuMA..",
+  },
+  { name: "Free Trial", href: "https://useast.cm.elasticpath.com/free-trial" },
 ];
 
 const company = [
-  { name: 'About Us', href: 'https://www.elasticpath.com/company/about-us' },
-  { name: 'Careers', href: 'https://www.elasticpath.com/company/careers' },
-  { name: 'Contact Us', href: 'https://www.elasticpath.com/company/contact-us' },
-  { name: 'Pricing', href: 'https://www.elasticpath.com/pricing' },
+  { name: "About Us", href: "https://www.elasticpath.com/company/about-us" },
+  { name: "Careers", href: "https://www.elasticpath.com/company/careers" },
+  {
+    name: "Contact Us",
+    href: "https://www.elasticpath.com/company/contact-us",
+  },
+  { name: "Pricing", href: "https://www.elasticpath.com/pricing" },
 ];
 
 function Safety({ className }) {
   return (
     <div
       className={clsx(
-        'flex h-24 max-w-[418px] overflow-clip rounded-2xl bg-white',
-        className
+        "flex h-24 max-w-[418px] overflow-clip rounded-2xl bg-white",
+        className,
       )}
-    >
-    </div>
+    ></div>
   );
 }
 
 function Status({ className }) {
   const [status, setStatus] = useState({
-    indicator: 'none',
-    description: 'All Systems Operational',
+    indicator: "none",
+    description: "All Systems Operational",
   });
 
   useEffect(() => {
-    if (typeof StatusPage !== 'undefined') {
+    if (typeof StatusPage !== "undefined") {
       // eslint-disable-next-line no-undef
-      var sp = new StatusPage.page({ page: 'wjlxrzb5h09l' });
+      const sp = new StatusPage.page({ page: "wjlxrzb5h09l" });
       sp.status({
         success: function (data) {
           setStatus({
@@ -87,15 +95,15 @@ function Status({ className }) {
     <Link
       href="https://status.elasticpath.com"
       className={clsx(
-        'flex items-center gap-2 rounded-lg border border-transparent p-1 px-2 font-jakarta font-semibold text-gray-500 transition-colors hover:border-gray-400 hover:bg-white hover:no-underline',
-        className
+        "flex items-center gap-2 rounded-lg border border-transparent dark:text-white text-black transition-colors font-normal hover:decoration-[#1FEC90]",
+        className,
       )}
       target="_blank"
     >
       <div
         className={clsx(
-          'h-4 w-4 rounded-full bg-[#2DB002]',
-          status.indicator === 'none' ? 'bg-[#2DB002]' : 'bg-yellow-500'
+          "h-4 w-4 rounded-full !bg-[#1FEC90] dark:shadow-[0px_0px_28px_0px_#1FEC90]",
+          status.indicator === "none" ? "bg-[#2DB002]" : "bg-yellow-500",
         )}
       ></div>
       <div>{status.description}</div>
@@ -106,14 +114,12 @@ function Status({ className }) {
 function Links({ name, links }) {
   return (
     <div>
-      <h3 className="font-jakarta text-base font-semibold uppercase text-gray-400">
-        {name}
-      </h3>
+      <h3 className="text-base font-medium dark:text-white/80">{name}</h3>
       <div className="flex flex-col gap-3">
         {links.map(({ name, href }) => (
           <Link
             href={href}
-            className="text-base text-gray-100 hover:text-primary hover:no-underline"
+            className="text-base dark:text-gray-100 text-black hover:text-primary hover:no-underline"
           >
             {name}
           </Link>
@@ -125,7 +131,7 @@ function Links({ name, links }) {
 
 export default function Footer() {
   return (
-    <footer className="bg-gray-950">
+    <footer className="relative z-10">
       <div className="mx-auto flex w-full max-w-7xl flex-col px-6 py-12">
         <div className="mb-12 flex flex-col gap-4 lg:flex-row lg:items-center lg:justify-between">
           <img src="/logo/dark.svg" alt="Dyte" className="h-9 w-fit lg:h-12" />
@@ -144,7 +150,7 @@ export default function Footer() {
           <Links name="Company" links={company} />
         </div>
 
-        <hr className="my-12 !bg-gray-300" />
+        <hr className="my-12 bg-white/30" />
 
         <div className="flex flex-col gap-6 lg:flex-row lg:items-center lg:justify-between lg:gap-0">
           <BrowserOnly>
@@ -154,7 +160,7 @@ export default function Footer() {
           </BrowserOnly>
           <Safety className="flex w-full max-w-full sm:hidden" />
 
-          <div className="flex flex-wrap gap-2 text-sm text-gray-500">
+          <div className="flex flex-wrap gap-2 text-sm dark:text-white">
             <Link
               href="https://www.elasticpath.com/company/privacy-policy"
               className="text-inherit hover:text-black hover:underline"
@@ -172,30 +178,6 @@ export default function Footer() {
             <span className="text-inherit">
               &copy; {new Date().getFullYear()} Elastic Path Software, Inc.
             </span>
-          </div>
-
-          <div className="flex items-center gap-4">
-            <Link
-              href="https://github.com/elasticpath"
-              aria-title="Elastic Path's GitHub Organization"
-            >
-              <Github className="h-7 w-7 text-zinc-400 hover:text-primary" />
-            </Link>
-            <Link
-              href="https://www.linkedin.com/company/elastic-path"
-              aria-title="LinkedIn"
-            >
-              <Linkedin className="h-7 w-7 text-zinc-400 hover:text-primary" />
-            </Link>
-            <Link href="https://twitter.com/elasticpath" aria-title="Elastic Path's Twitter">
-              <XIcon className="h-7 w-7 text-zinc-400 hover:text-primary" />
-            </Link>
-            <Link
-              href="https://www.youtube.com/c/elasticpathsoftware"
-              aria-title="Elastic Path YouTube Channel"
-            >
-              <Youtube className="h-7 w-7 text-zinc-400 hover:text-primary" />
-            </Link>
           </div>
         </div>
       </div>
