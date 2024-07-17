0e72733faf240da1bf13195966fad25316b74b16
David Stover <56250584+davidmoltin@users.noreply.github.com>
2024-07-17 11:35:40 -0400

    Add authentication to all specs

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -154,8 +154,8 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: "facebook", // Usually your GitHub org/user name.
-  projectName: "docusaurus", // Usually your repo name.
+  organizationName: "ElasticPath", // Usually your GitHub org/user name.
+  projectName: "elasticpath-dev", // Usually your repo name.
   markdown: {
     mermaid: true,
   },
@@ -274,45 +274,6 @@ const config = {
       footer: {
         style: "dark",
         links: [
-          {
-            title: "Docs",
-            items: [
-              {
-                label: "Tutorial",
-                to: "/docs/intro",
-              },
-            ],
-          },
-          {
-            title: "Community",
-            items: [
-              {
-                label: "Stack Overflow",
-                href: "https://stackoverflow.com/questions/tagged/docusaurus",
-              },
-              {
-                label: "Discord",
-                href: "https://discordapp.com/invite/docusaurus",
-              },
-              {
-                label: "Twitter",
-                href: "https://twitter.com/docusaurus",
-              },
-            ],
-          },
-          {
-            title: "More",
-            items: [
-              {
-                label: "Blog",
-                to: "/blog",
-              },
-              {
-                label: "GitHub",
-                href: "https://github.com/facebook/docusaurus",
-              },
-            ],
-          },
         ],
         copyright: `Copyright © ${new Date().getFullYear()} Elastic Path Software`,
       },
@@ -386,16 +347,6 @@ const config = {
             outputDir: "docs/api/addresses",
             downloadUrl:
               "https://raw.githubusercontent.com/elasticpath/elasticpath-dev/main/openapispecs/addresses/AccountAddresses.yaml",
-            sidebarOptions: {
-              groupPathsBy: "tag",
-              categoryLinkSource: "tag",
-            },
-          },
-          authentication: {
-            specPath: "openapispecs/authentication/OpenAPISpec.yaml",
-            outputDir: "docs/api/authentication",
-            downloadUrl:
-              "https://raw.githubusercontent.com/elasticpath/elasticpath-dev/main/openapispecs/authentication/OpenAPISpec.yaml",
             sidebarOptions: {
               groupPathsBy: "tag",
               categoryLinkSource: "tag",
