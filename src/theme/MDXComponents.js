71ac0386dbdb8901c336f90918d45584ae26cf69
David Stover <56250584+davidmoltin@users.noreply.github.com>
2024-08-27 19:57:27 -0400

    Revert "Remove MDX Components"
    This reverts commit 1eeb88025acb642aff8389768c0686f1e5f563d7.

--- src/theme/MDXComponents.js
+++ src/theme/MDXComponents.js
@@ -1,8 +1,4 @@
 // Import the original mapper
-import MDXComponents from '@theme-original/MDXComponents';
-
-import Tabs from '@theme/Tabs';
-import TabItem from '@theme/TabItem';
 import ColorCode from '../components/ColorCode';
 import ColorPalette from '../components/ColorPalette';
 import VideoPlayer from '../components/VideoPlayer';
@@ -14,11 +10,7 @@ import * as icons from '../icons';
 
 export default {
   // Re-use the default mapping
-  ...MDXComponents,
   ...icons,
-
-  Tabs,
-  TabItem,
 
   color: ColorCode,
   ColorPalette,
