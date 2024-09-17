e2faa0810678c898573b8271d9d65d1c8a51e621
David Stover <56250584+davidmoltin@users.noreply.github.com>
2024-09-17 14:08:27 -0400

    Add video support

--- src/components/EmbedVideo/index.jsx
+++ src/components/EmbedVideo/index.jsx
@@ -1,27 +1,22 @@
 import React from 'react';
-import DOMPurify from 'dompurify';
 
 const EmbedVideo = ({ videoUrl }) => {
   let embedUrl = '';
 
-  // Sanitize video URL using DOMPurify
-  const sanitizedUrl = DOMPurify.sanitize(videoUrl);
-
   // Check if it's a YouTube or Vidyard URL and convert it to an embeddable format
-  if (sanitizedUrl.includes('youtube.com') || sanitizedUrl.includes('youtu.be')) {
-    const videoId = sanitizedUrl.split('v=')[1]?.split('&')[0] || sanitizedUrl.split('/').pop();
+  if (videoUrl.includes('youtube.com') || videoUrl.includes('youtu.be')) {
+    const videoId = videoUrl.split('v=')[1]?.split('&')[0] || videoUrl.split('/').pop();
     embedUrl = `https://www.youtube.com/embed/${videoId}`;
-  } else if (sanitizedUrl.includes('vidyard.com')) {
-    const videoId = sanitizedUrl.split('/').pop();
+  } else if (videoUrl.includes('vidyard.com')) {
+    const videoId = videoUrl.split('/')[videoUrl.split('/').length - 1];
     embedUrl = `https://play.vidyard.com/${videoId}.html`;
   }
 
   return (
     <iframe
       className="w-full h-full object-cover"
-      src={DOMPurify.sanitize(embedUrl)} // Sanitize the embed URL
+      src={embedUrl}
       title="Embedded video"
-      frameBorder="0"
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
       allowFullScreen
     />
--- src/components/homepage/ui/AbstractBG.tsx
+++ src/components/homepage/ui/AbstractBG.tsx
@@ -3,9 +3,7 @@ import React from "react";
 export default function () {
   return (
     <div className="z-0 absolute w-[100vw] h-full">
-      <div className="dark:bg-[url('/assets/homepage/DARK-BLEND.webp')] fixed top-0 left-0 w-screen bg-cover bg-center z-[-1]"></div>
-      <div className="fixed top-0 left-0 w-screen bg-cover bg-center z-[-1] dark:bg-[url('/assets/homepage/DARK-BLEND.webp')]"></div>
-
+      <div className="w-full h-full dark:bg-[url('/assets/homepage/blenddark2.webp')] bg-center bg-no-repeat bg-cover"></div>
     </div>
   );
 }
