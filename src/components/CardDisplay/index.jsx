4cac28db6a1ea74fe6697ed82ac82b8bf7b2b32d
David Stover <56250584+davidmoltin@users.noreply.github.com>
2024-09-18 15:53:59 -0400

    Add video player support with Youtube, Vidyard and Vimeo.

--- src/components/CardDisplay/index.jsx
+++ src/components/CardDisplay/index.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import EmbedVideo from '../EmbedVideo'; // Import the new EmbedVideo component
 
-const CardDisplay = ({ mediaSrc, mediaType, title, description, link }) => {
+const CardDisplay = ({ mediaSrc, mediaType, title, description, link, className }) => {
   return (
     <a
       href={link}
-      className="block rounded-3xl overflow-hidden hover:bg-gray-100 hover:shadow-md shadow-lg transition-shadow duration-200 no-underline hover:no-underline dark:bg-gray-900"
+      className={`block rounded-3xl overflow-hidden hover:bg-gray-100 hover:shadow-md shadow-lg transition-shadow duration-200 no-underline hover:no-underline dark:bg-gray-900 ${className}`}
     >
       <div className="aspect-video">
         {mediaType === 'video' ? (
@@ -19,10 +19,12 @@ const CardDisplay = ({ mediaSrc, mediaType, title, description, link }) => {
           <img className="w-full h-full object-cover" src={mediaSrc} alt={title} />
         )}
       </div>
-      <div className="px-6 py-4">
-        <div className="font-bold text-xl mb-2">{title}</div>
-        <p className="text-gray-700 text-base">{description}</p>
+      {(title || description) && (
+        <div className="px-4 py-2">
+          <div className="font-bold text-xl mb-2">{title}</div>
+          <div className="text-gray-700 dark:text-white">{description}</div>
       </div>
+        )}
     </a>
   );
 };
