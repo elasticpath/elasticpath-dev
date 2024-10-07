8fc87154c561cfa71729e8eece24f25f0f5ee9d9
David Stover <56250584+davidmoltin@users.noreply.github.com>
2024-10-07 14:56:05 -0400

    Update video for float

--- src/components/CardDisplay/video.jsx
+++ src/components/CardDisplay/video.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import EmbedVideo from '../EmbedVideo'; // Import the new EmbedVideo component
 
 const Video = ({ mediaSrc, mediaType, title, float, size }) => {  return (
-  <div className={`w-[${size}] ${float}`}>
+
     <div
       className={`block aspect-video rounded-3xl overflow-hidden p-0 m-6 shadow-lg no-underline hover:no-underline dark:bg-gray-900`}
     >
@@ -19,7 +19,6 @@ const Video = ({ mediaSrc, mediaType, title, float, size }) => {  return (
         )}
       </div>
     </div>
-  </div>
 );
 };
 
